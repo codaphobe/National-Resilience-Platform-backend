7fae4ed3c8af8b9b4386a671bec74df6cb4e5caf
codaphobe <kutakanakerisanjay@gmail.com>
2025-01-19 18:51:39 +0530

    Added CORS

--- server.js
+++ server.js
@@ -5,25 +5,26 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 
-// Load environment variables
-dotenv.config();
-
 // Connect to MongoDB
 connectdb();
 
+dotenv.config();
+
+const allowedOrigins = process.env.CORS_ORIGINS.split(',');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-const allowedOrigins = [
-    'http://localhost:5173/',
-    'https://nationalcrisis.netlify.app/'
-]
-
 const corsOptions = {
-    origin:true,
+    function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type','Authorization'],
     credentials: true,
@@ -59,8 +60,6 @@ app.get('/api/protected', verifyToken, (req, res) => {
 app.get('/api/test', (req, res) => {
     res.json({ message: 'This is a test route' });
 });
-
-// Routes
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
